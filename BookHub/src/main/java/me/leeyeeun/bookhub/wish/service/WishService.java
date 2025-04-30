c63a36f07115693188129e2b8b022306ac9086cc
Lee, yeeun <ye7426@naver.com>
2025-04-30 11:10:28 +0900

    feature/#27(WishService): 책 장르 조회 작성

--- BookHub/src/main/java/me/leeyeeun/bookhub/wish/service/WishService.java
+++ BookHub/src/main/java/me/leeyeeun/bookhub/wish/service/WishService.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import me.leeyeeun.bookhub.member.entity.Member;
 import me.leeyeeun.bookhub.member.repository.MemberRepository;
 import me.leeyeeun.bookhub.wish.controller.dto.request.WishRequestDto;
+import me.leeyeeun.bookhub.wish.controller.dto.response.CategoryPercentageResponseDto;
+import me.leeyeeun.bookhub.wish.entity.Category;
 import me.leeyeeun.bookhub.wish.entity.Wish;
 import me.leeyeeun.bookhub.wish.repository.WishRepository;
 import org.springframework.stereotype.Service;
@@ -12,7 +14,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -91,6 +96,7 @@ public class WishService {
         return wishRepository.findByBooknameContainingIgnoreCase(keyword);
     }
 
+    // 월별 읽은 책 개수 통계
     @Transactional(readOnly = true)
     public long countWishesByYearAndMonth(Principal principal, int year, int month) {
         Member member = getMemberFromPrincipal(principal);
@@ -103,5 +109,29 @@ public class WishService {
                 startOfMonth,
                 endOfMonth
         ).size();
+    }
+
+    // 장르 통계
+    @Transactional(readOnly = true)
+    public List<CategoryPercentageResponseDto> getCategoryGenre(Principal principal) {
+        Member member = getMemberFromPrincipal(principal);
+
+        List<Wish> allWishes = wishRepository.findAllByMemberId(member.getId());
+
+        int total = allWishes.size();
+        if (total == 0) return List.of(); // 위시글 없으면 빈 리스트
+
+        Map<Category, Long> countByCategory = allWishes.stream()
+                .filter(w -> w.getCategory() != null)
+                .collect(Collectors.groupingBy(Wish::getCategory, Collectors.counting()));
+
+        // 퍼센트로 변환
+        return countByCategory.entrySet().stream()
+                .map(entry -> new CategoryPercentageResponseDto(
+                        entry.getKey(),
+                        Math.round((entry.getValue() * 100.0 / total) * 10) / 10.0 // 소수점 첫째 자리까지 나타냄 (반올림)
+                ))
+                .sorted(Comparator.comparing(CategoryPercentageResponseDto::percentage).reversed()) // 내림차순
+                .toList();
     }
 }
