f05cf0235815a9c1d38af8d06f6a67641c980e66
Lee, yeeun <ye7426@naver.com>
2025-06-04 14:06:44 +0900

    refactor/#64(Controller): Valid 누락된 부분 추가

--- BookHub/src/main/java/me/leeyeeun/bookhub/wish/controller/dto/request/WishRequestDto.java
+++ BookHub/src/main/java/me/leeyeeun/bookhub/wish/controller/dto/request/WishRequestDto.java
@@ -1,20 +1,27 @@
 package me.leeyeeun.bookhub.wish.controller.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import me.leeyeeun.bookhub.wish.entity.Category;
 import me.leeyeeun.bookhub.wish.entity.Progress;
 import me.leeyeeun.bookhub.wish.entity.Star;
 
 public record WishRequestDto(
+        @NotBlank(message = "필수 입력값입니다.")
         String bookname,
 
+        @NotBlank(message = "필수 입력값입니다.")
         String author,
 
+        @NotBlank(message = "필수 입력값입니다.")
         String content,
 
+        @NotBlank(message = "필수 입력값입니다.")
         Progress progress,
 
+        @NotBlank(message = "필수 입력값입니다.")
         Category category,
 
+        @NotBlank(message = "필수 입력값입니다.")
         Star star
 ) {
 }
