a18900996153ab219095e5470f36dae5c6062a1c
Lee, yeeun <ye7426@naver.com>
2025-06-21 22:14:05 +0900

    refactor/#80(Bookmark): Bookmark 생성일 추가하기 & Community 생성일도 수정

--- BookHub/src/main/java/me/leeyeeun/bookhub/bookmark/controller/dto/response/BookmarkResponseDto.java
+++ BookHub/src/main/java/me/leeyeeun/bookhub/bookmark/controller/dto/response/BookmarkResponseDto.java
@@ -1,5 +1,6 @@
 package me.leeyeeun.bookhub.bookmark.controller.dto.response;
 
+import me.leeyeeun.bookhub.bookmark.entity.Bookmark;
 import me.leeyeeun.bookhub.community.entity.Community;
 
 import java.time.LocalDateTime;
@@ -8,16 +9,19 @@ public record BookmarkResponseDto(
         Long id,
         String title,
         String nickname,
-        LocalDateTime createdat
+        LocalDateTime communityCreatedAt, // 커뮤니티글 작성일
+        LocalDateTime bookmarkedAt // 북마크 생성일
 ) {
-    public static BookmarkResponseDto from(Community community) {
+    public static BookmarkResponseDto from(Bookmark bookmark) {
+        Community community = bookmark.getCommunity();
         String nickname = (community.getMember() != null) ? community.getMember().getNickname() : "별명 없음";
 
         return new BookmarkResponseDto(
                 community.getId(),
                 community.getTitle(),
                 nickname,
-                community.getCreatedat()
+                community.getCreatedAt(), // 커뮤니티글 작성일
+                bookmark.getCreatedAt() // 북마크 생성일
         );
     }
 }
